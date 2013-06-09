8ccbc19844053f38e94418b6f27dfe34598fd738
Brian Genisio <briangenisio@gmail.com>
2013-06-09 14:26:27 -0400

    added about page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,53 @@
-d3.json('/videos.json', function(error, data) {
-  var navs = d3.select('.lessons')
-    .selectAll('li')
-    .data(data);
+(function() {
+	d3.json('/videos.json', function(error, data) {
+	  var navs = d3.select('.lessons-nav .lessons')
+	    .selectAll('li')
+	    .data(data);
 
-  var action = navs.enter()
-    .append('li')
-    .append('a');
+	  var action = navs.enter()
+	    .append('li')
+	    .append('a');
 
-  action.attr('href', '#')
-    .text(function(d) { return d.title; });
+	  action.attr('href', '#')
+	    .text(function(d) { return d.title; });
 
-  action.on('click', function(d) {
-  	navs.attr('class', '');
-  	var selected = d3.select(d3.event.toElement.parentElement);
+	  action.on('click', function(d) {
+	  	navs.classed('active', false);
+	  	d3.select(d3.event.toElement.parentElement)
+	  		.classed('active', true);
 
-  	selected.attr('class', 'active');
+	  	d3.select('.title').text(d.title);
 
-  	d3.select('.title').text(d.title);
+	    d3.select('.youtube iframe')
+	      .attr('src', 'http://www.youtube.com/embed/' + d.youtube);
 
-    d3.select('.youtube iframe')
-      .attr('src', 'http://www.youtube.com/embed/' + d.youtube);
+	    d3.select('.codepen')
+	    	.classed('hidden', false)
+	    	.select('a')
+	    	.attr('href', 'http://codepen.io/BrianGenisio/pen/' + d.codepen)
 
-    d3.select('.codepen')
-    	.attr('style', 'display: block;')
-    	.select('a')
-    	.attr('href', 'http://codepen.io/BrianGenisio/pen/' + d.codepen)
+	  });
+	});
 
-  });
-});
+	var navBar = d3.select('.navbar .nav');
+	var lessons = navBar.select('.lessons');
+	var about = navBar.select('.about');
+
+	function selectNav() {
+		navBar.selectAll('li').classed('active', false);
+		d3.select(d3.event.toElement.parentElement)
+			.classed('active', true);
+	}
+
+	lessons.on('click', function() {
+		selectNav();
+		d3.select('.lessons-container').classed('hidden', false);
+		d3.select('.about-container').classed('hidden', true);
+	});
+
+	about.on('click', function() {
+		selectNav();
+		d3.select('.lessons-container').classed('hidden', true);
+		d3.select('.about-container').classed('hidden', false);
+	});
+})();

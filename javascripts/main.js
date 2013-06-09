65c54cf8001e8288e3d47dba6b7994a2189af180
Brian Genisio <briangenisio@gmail.com>
2013-06-09 13:25:22 -0400

    adding title and active selections

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+d3.json('/videos.json', function(error, data) {
+  var navs = d3.select('.lessons')
+    .selectAll('li')
+    .data(data);
+
+  var action = navs.enter()
+    .append('li')
+    .append('a');
+
+  action.attr('href', '#')
+    .text(function(d) { return d.title; });
+
+  action.on('click', function(d) {
+  	navs.attr('class', '');
+  	var selected = d3.select(d3.event.toElement.parentElement);
+
+  	selected.attr('class', 'active');
+
+  	d3.select('.title').text(d.title);
+
+    d3.select('.youtube iframe')
+      .attr('src', 'http://www.youtube.com/embed/' + d.youtube);
+  });
+});
